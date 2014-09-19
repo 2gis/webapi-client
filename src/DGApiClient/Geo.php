763b7af1fbec6fc061469db70b40fff1fb6963c2
Vitaly Kulpin <v.kulpin@2gis.ru>
2014-09-19 12:28:56 +0700

    Добавлен еще один метод к геокодеру. Поиск в радиусе с заданным центром

--- src/DGApiClient/Geo.php
+++ src/DGApiClient/Geo.php
@@ -33,4 +33,39 @@ class Geo extends AbstractDomainClient
             )
         );
     }
+
+
+    /**
+     * Поиск гео объектов в радиусе, с заданным центром
+     * @param $point
+     * @param int $radius
+     * @param int $page
+     * @param string[] $type
+     * @param int $page
+     * @param int $pageSize
+     * @param string[] $additionalFields
+     * @throws Exceptions\Exception
+     * @return \DGApiClient\Mappers\Geo\GeoObject[]
+     */
+     public function geoSearch(
+            $point,
+            $radius = 10,
+            $type = array(),
+            $page = null,
+            $pageSize = null,
+            array $additionalFields = array()
+        ) {
+            return $this->getInternalList(
+                'geo/search',
+                array(__NAMESPACE__ . '\Mappers\Geo\GeoObject', 'geoMapResolver'),
+                array(
+                    'point' => $point,
+                    'radius' => $radius,
+                    'type' => self::getArray($type),
+                    'page' => $page,
+                    'page_size' => $pageSize,
+                    'fields' => self::getArray($additionalFields),
+                )
+            );
+        }
 }
